51ea3541a42f0826ff821be37ce345ce7892abbe
ouss-hafsi <oussema.ben.hafsi@gmail.com>
2023-12-19 13:17:13 +0100

    landing page updates

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 console.log('connected')
 
+//               Landing Page
 paceOptions = {
     ajax: true,
     document: true,
@@ -8,23 +9,8 @@ paceOptions = {
 
     Pace.on('done', function() {
     $('.p').delay(500).animate({top: '30%', opacity: '0'}, 3000, $.bez([0.19,1,0.22,1]));
-
-
     $('#preloader').delay(1500).animate({top: '-100%'}, 2000, $.bez([0.19,1,0.22,1]));
-
-    // TweenMax.from(".title", 2, {
-    //      delay: 1.8,
-    //           y: 10,
-    //           opacity: 0,
-    //           ease: Expo.easeInOut
-    //     })
    });
-
-
-
-
-
-
 
 const snapPoints = [0, 213];
 let dragMe = Draggable.create(".dragme", {
@@ -42,20 +28,18 @@ function getThePosition() {
         document.querySelector('#img-1').style.opacity = 0
         document.querySelector('#img-2').autoplay = true
         document.querySelector('#img-2').currentTime = 0
-        document.querySelector('.btn').style.display = "block"
+        document.querySelector('.btn-continue').style.display = "block"
         document.querySelector('.dragme-child').style.display = "block"
         document.querySelector('.arrow').style.display = "none"
         document.querySelector('.dragme-text').style.opacity= 0
-        // document.querySelector('.circle-copy').style.background = 'white'
         TweenLite.to('.dragme', {
             y: 213
         })
     } else {
         document.querySelector('#img-1').style.opacity = 1
-        document.querySelector('.btn').style.display = 'none'
+        document.querySelector('.btn-continue').style.display = 'none'
         document.querySelector('.dragme-child').style.display = "none"
         document.querySelector('.arrow').style.display = "block"
-        // document.querySelector('.circle-copy').style.background = '#d200e6'
         document.querySelector('.dragme-text').style.opacity= 1
         TweenLite.to('.dragme', {
             y: 0
