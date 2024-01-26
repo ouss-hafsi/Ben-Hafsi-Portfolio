1db95e9bd257fcd5fdd96f2adaf07e4f30e8ac41
ouss-hafsi <oussema.ben.hafsi@gmail.com>
2024-01-25 20:10:03 -0800

    testing landing page

--- script.js
+++ script.js
@@ -17,16 +17,16 @@ continueBtn.addEventListener('click', function() {
 })
 
 //               Landing Page
-// paceOptions = {
-//     ajax: true,
-//     document: true,
-//     eventLag: false
-//     };
+paceOptions = {
+    ajax: true,
+    document: true,
+    eventLag: false
+    };
 
-//     Pace.on('done', function() {
-//     $('.p').delay(500).animate({top: '30%', opacity: '0'}, 3000, $.bez([0.19,1,0.22,1]));
-//     $('#preloader').delay(1500).animate({top: '-100%'}, 2000, $.bez([0.19,1,0.22,1]));
-//    });
+    Pace.on('done', function() {
+    $('.p').delay(500).animate({top: '30%', opacity: '0'}, 3000, $.bez([0.19,1,0.22,1]));
+    $('#preloader').delay(1500).animate({top: '-100%'}, 2000, $.bez([0.19,1,0.22,1]));
+   });
 
 
 //                 Drag Button
